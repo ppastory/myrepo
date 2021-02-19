81d1bb98172e29aace8e1ca7c3cb9fd75aefba8c
Pablo Vicente Pastor Y Camarasa <Pablo.Pastor@UGent.be>
2021-02-19 15:34:13 +0100

    some initial cleaning

--- LS_Cigarette.R
+++ LS_Cigarette.R
@@ -9,12 +9,19 @@
 rm(list = ls())   # Clear workspace
 
 ## Set input file directory
-setwd("/Users/geeverae/dropbox/My_doc/Onderwijs/Advanced Econometrics/R/Sample R code") #Mac
+
+#let's check what is the current directory
+getwd()
+#it's fiiine !
+
+#We don't have to sent the current directory because it is our directory myrepo
+#C:/Users/ppastory/Documents/programming/myrepo
 #setwd("C:\\Users\\...") #Windows
 
 ## Set output file directory: change to your own directory
-output="/Users/geeverae/dropbox/My_doc/Onderwijs/Advanced Econometrics/R/Sample R code" #Mac
-#output = "C:\\Users\\..." #Windows
+#I want the output directory to be the same 
+output = getwd() 
+#so now everything is in one directory
 
 ## Install required packages (code is only executed if the packages are not yet installed)
 if(!require(plm)){install.packages("plm")} ## panel data package
@@ -54,11 +61,19 @@ lnY_it_D    <- pdt_noNaN$ln.Y_it
 y_D<-as.vector(lnC_it_D)
 x_D<-as.matrix(cbind(Cnst=1,lnC_itL1_D,lnP_it_D,lnPn_it_D,lnY_it_D))
 
+#ok now we've got all the data we need!
+
 ####################################################################
 ###                Run OLS on static model                       ###
 ####################################################################
 
 OLS_static = OLS_own(y_S,x_S)
+
+#let's check the coefficients
+OLS_static[,1]
+#let's check the p-values
+OLS_static[,4]
+#they are all significant !
 
 
 ####################################################################
