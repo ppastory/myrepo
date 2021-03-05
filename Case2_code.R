04d0accb598d90653c83ad82092b5131b740eef4
Pablo Vicente Pastor Y Camarasa <Pablo.Pastor@UGent.be>
2021-03-05 17:24:21 +0100

    case 2.1 (not X stochastic)

--- Case2_code.R
+++ Case2_code.R
@@ -35,13 +35,38 @@ library(plm)
 source("Case1_Functions.R")
 
 ####################################################################
-### Monte Carlo simulation for mean of Chi2 distributed variable
+#Case 2.1. Monte Carlo simulation for mean of Chi2 distributed variable
 ####################################################################
 
 #Setting the seeds so that the simulation gives us the same results
 set.seed(123)
 #n is the size of the sample
-T <- 25
+sz <- c(25,50,100,500,2500)
+
+
+################################################
+####Let's loop over different sample size ######
+################################################
+
+#there is one table for beta0
+
+table_final_beta0 <- matrix(0,length(sz),8)
+ 
+colnames(table_final_beta0) <- c("Sample size","population","MC","std_analytical","std_num","std_estimated","Var_ana","Var_estimated")
+ 
+table_final_beta1 <- matrix(0,length(sz), 10)
+
+colnames(table_final_beta1) <- c("Sample size","population","MC","std_analytical","std_num","std_estimated","Var_ana","Var_estimated","2.5%","97.5%")
+
+table_final_sp <- matrix(0,length(sz), 6)
+
+colnames(table_final_sp) <- c("Sample size","size","power B=0.95","power B=0.9","power B=0.75","power B=0.5")
+
+iter <- 0
+
+for (T in sz){
+  iter <- iter + 1
+  
 #repl number of replication
 repl <- 10000
 #df is the number of degrees of freedom
@@ -87,7 +112,7 @@ for (j in 1:length(beta1_test)) {
 
   for (i in 1:repl) {
   #stochastic X: X = rnorm(T,0,sigma2)
-  #let's get some errors
+  #let's get some errors, we define sigma 2 =1 earlier
   e <- rnorm(T,0,sigma2)
   #now I can have my y !
   Y <- X%*%beta + e
@@ -101,8 +126,7 @@ for (j in 1:length(beta1_test)) {
   stdvs_0[i] <- OLS_out$estimation[1,2]/sqrt(T)
   stdvs_1[i] <- OLS_out$estimation[2,2]/sqrt(T)
   
-  ttest_matrix[i,j] <- (beta_1[i] - beta1_test[j])/stdvs_1[i]
-  print(i,j)
+  ttest_matrix[i,j] <- (beta_1[i] - beta1_test[j])/OLS_out$estimation[2,2]
   }
 
 }
@@ -112,40 +136,104 @@ colnames(ttest_matrix) <- c(1,0.95,0.90,0.75,0.5)
 
 beta_0_bar <- mean(beta_0)
 beta_1_bar <- mean(beta_1)
+#let's get the numerical standard errors
+stdvs_0_num <- sqrt(var(beta_0))/sqrt(T)
+stdvs_1_num <- sqrt(var(beta_1))/sqrt(T)
+
+var_0_num <- var(beta_0)/T
+var_1_num <- var(beta_1)/T
+
+
+#Let get analytical std dev, on a SMALL sample
+OLS_std <- OLS_own(Y,X,0)
+stdvs_0_ana <- OLS_std$estimation[1,2]/sqrt(T) 
+stdvs_1_ana <- OLS_std$estimation[2,2]/sqrt(T)
+
+
+x<-X
+xxi    <- solve(t(x)%*%x) #this is (X' X)^(-1)
+var_01_ana  <- sigma2*diag(xxi)/T
+var_0_ana <- var_01_ana[1]
+var_1_ana <- var_01_ana[2]
+
 stdvs_0_bar <- mean(stdvs_0)
 stdvs_1_bar <- mean(stdvs_1)
 
+var_0_bar <- mean(stdvs_0)^2
+var_1_bar <- mean(stdvs_1)^2
+
+
+
+#let's see what are the true standard standars
+table_std <- rbind(cbind(stdvs_0_ana,stdvs_0_num,stdvs_0_bar),cbind(stdvs_1_ana,stdvs_1_num,stdvs_1_bar))
+colnames(table_std) <- c("analytical","numerical","estimated")
+table_std
+
+#let's see what are the true variance
+table_var <- rbind(cbind(var_0_ana,var_0_num,var_0_bar),cbind(var_1_ana,var_1_num,var_1_bar))
+colnames(table_var) <- c("analytical","numerical","estimated")
+table_var
+
+
 #let's do a little table that compares the MC with the population
-table <- rbind(cbind(b_0,beta_0_bar),cbind(b_1,beta_1_bar),cbind(0,stdvs_0_bar),cbind(0,stdvs_1_bar))
+table_beta <- cbind(b_0,beta_0_bar,b_1,beta_1_bar)
 
-colnames(table) <- c("population","montecarlo")
-table
+colnames(table_beta) <- c("Beta_0_pop","Beta_0_MC","Beta_1_pop","Beta_1_MC")
+table_beta <- cbind(T,table_beta)
 
-#calculate the size
+#let's store the betas in a table
 
-hist(ttest_matrix[,4])
+beta_matrix[iter,] <- cbind(table_beta)
+
 
 #loop over the t-tests and give me the Critical values for each one
 
 #let's do a matrix of critical values
 
-CV_beta1 <- matrix(0,2,5)
-
-for (j in 1:5) {
-  print(j)
-  CV_beta1[1,j] <- quantile(ttest_matrix[,j], c(.025))
-  CV_beta1[2,j] <- quantile(ttest_matrix[,j], c(.0975))
-}
+CV_beta1_LB <- quantile(ttest_matrix[,1], c(.025))
+CV_beta1_UB <- quantile(ttest_matrix[,1], c(.975))
 
 #
 
-mean_X_bar = mean(X_bar)
-var_X_bar  = var(X_bar)
-mean_est_var_X_bar = mean(X_var)
-skew_X_bar = mean(((X_bar-mean_X_bar)/sqrt(var_X_bar))^3)
-kurt_X_bar = mean(((X_bar-mean_X_bar)/sqrt(var_X_bar))^4)
-jb_X_bar = (repl/6)*(skew_X_bar^2 + (1/4)*(kurt_X_bar-3)^2)
-p_jb_X_bar = pchisq(jb_X_bar, df=2, lower.tail=FALSE)
+rej_function <- function(ttest,LB,UB){
+  if (ttest <= LB || ttest >= UB){
+    return(1)
+  }else{
+    return(0)
+  }
+}
+
+#initialise matrix of rejection 
+rej_matrix <- matrix(0,nrow(ttest_matrix),ncol(ttest_matrix))
+#Lets store the 1 and 0 of rejection in a matrix
+for (j in 1:5){
+  rej_matrix[,j]<-sapply(ttest_matrix[,j],rej_function, LB= CV_beta1_LB,UB= CV_beta1_UB)
+}
+#this is the size, the mean of column 1 for which beta =1
+size_beta1 <- mean(rej_matrix[,1])
+#size_beta1
+
+#the power is P(non reject if Beta != 1) -> 1 - P(reject)
+power_beta1 <- 1 -colMeans(rej_matrix[,2:5])
+#power_beta1
+
+#this is the table in which you have all beta_0
+table_final_beta0[iter,] <- cbind(T,b_0,beta_0_bar,stdvs_0_ana,stdvs_0_num,stdvs_0_bar,var_0_ana,var_0_bar)
+
+#this is the table in which you have all beta_1
+table_final_beta1[iter,] <- cbind(T,b_1,beta_1_bar,stdvs_1_ana,stdvs_1_num,stdvs_1_bar,var_1_ana,var_1_bar,CV_beta1_LB,CV_beta1_UB)
+
+#table with size and power
+table_final_sp[iter,] <- cbind(T,size_beta1,t(power_beta1))
+
+}
+
+
+
+
+
+
+
 
 ####################################################################
 ### Bootstrap simulation for mean of Chi2 distributed variable
