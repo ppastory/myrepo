4d71b469dfe0615ca02b1ee22e0f45ca6c727fa9
Pablo Vicente Pastor Y Camarasa <Pablo.Pastor@UGent.be>
2021-03-08 18:36:59 +0100

    OLS, OLSW, GLS, still need EGLS heterostyle

--- Case2.2_code.R
+++ Case2.2_code.R
@@ -274,11 +274,11 @@ colnames(sp_mat) <- c("size","power B=0.95","power B=0.9","power B=0.75","power 
   stdvs_1_bar <- mean(stdvs_1)
   
   
-  #first row of OLS
+  #second row of OLSW
   table_beta0 <- cbind(b_0,beta_0_bar,stdvs_0_ana,stdvs_0_num,stdvs_0_bar)
   colnames(table_beta0) <- c("population","estimated beta","analytical","numerical","estimated")
   
-  #first row of OLS
+  #second row of OLSW
   table_beta1 <- cbind(b_1,beta_1_bar,stdvs_1_ana,stdvs_1_num,stdvs_1_bar)
   colnames(table_beta1) <- c("population","estimated beta","analytical","numerical","estimated")
   
@@ -290,16 +290,7 @@ colnames(sp_mat) <- c("size","power B=0.95","power B=0.9","power B=0.75","power 
   
   CV_beta1_LB <- quantile(ttest_matrix[,1], c(.025))
   CV_beta1_UB <- quantile(ttest_matrix[,1], c(.975))
-  
-  #
-  
-  rej_function <- function(ttest,LB,UB){
-    if (ttest <= LB || ttest >= UB){
-      return(1)
-    }else{
-      return(0)
-    }
-  }
+
   
   #initialise matrix of rejection 
   rej_matrix <- matrix(0,nrow(ttest_matrix),ncol(ttest_matrix))
@@ -485,6 +476,180 @@ colnames(sp_mat) <- c("size","power B=0.95","power B=0.9","power B=0.75","power 
   
   #table beta1
   beta_1_mat[3,] <-table_beta1
+  
+  
+  
+  ################################################
+  ####EGLS case ######
+  ################################################
+  
+  
+  #I am initialising the vectors in which beta and other stuff will arrive
+  beta_0 <- rep(0,repl)
+  beta_1 <- rep(0,repl)
+  stdvs_0 <- rep(0,repl)
+  stdvs_1 <- rep(0,repl)
+  
+  #grid of beta1 
+  beta1_test <- as.matrix(t(c(1,0.95,0.90,0.75,0.5)))
+  
+  #ttest matrix with column are different beta1 and 
+  #the rows are one ttest per simulation
+  
+  ttest_matrix <- matrix(0,repl, length(beta1_test))
+  
+  for (j in 1:length(beta1_test)) {
+    
+    for (i in 1:repl) {
+      #stochastic X: X = rnorm(T,0,sigma2)
+      #let's get some errors, we define sigma 2 =1 earlier
+      e <- rnorm(T,0,sigma2)
+      #now I can have my y !
+      Y <- X%*%beta + e
+      
+      
+      OLS_static = OLS_own(Y,X,0)
+      
+      res <- OLS_static$residuals
+      k <- OLS_static$param
+      
+      #We have 30 error terms per states
+      t <- 30
+      
+      #There is one different sigma per state (46 states)
+      sigma_i <- seq(1:46)
+      
+      
+      #let's compute the sigma_i 
+      for (i in 1:(length(res)/t)){
+        sigma_i[i] <- (t(res[(1+(i-1)*t):(i*t)])%*%(res[(1+(i-1)*t):(i*t)]))/(t-k) 
+      }
+      #Maybe K is 46 but not sure
+      
+      #Now we create the diagonal of sigma_hat
+      
+      sigma_est<-rep(sigma_i,each=t)
+      
+      #now we create the matrix and put sigma_hat as the diagonal of that matrix
+      
+      omega_hat <- matrix(0,length(res), length(res)) 
+      #I put back the diagonal element in the diagnonals
+      diag(omega_hat) <- sigma_est
+      
+      omega_hat
+      
+      GLS_static = GLS_own (Y,X,omega_hat)
+      
+      
+      beta_0[i] <- GLS_static[1,1]
+      beta_1[i] <- GLS_static[2,1]
+      
+      stdvs_0[i] <- GLS_static[1,2]/sqrt(T)
+      stdvs_1[i] <- GLS_static[2,2]/sqrt(T)
+      
+      ttest_matrix[i,j] <- (beta_1[i] - beta1_test[j])/stdvs_1[i] #divided by sqrt T
+    }
+    
+  }
+  
+  colnames(ttest_matrix) <- c(1,0.95,0.90,0.75,0.5)
+  
+  
+  beta_0_bar <- mean(beta_0)
+  beta_1_bar <- mean(beta_1)
+  
+  #let's get the numerical standard errors -> truuuue
+  
+  var_0_num <- var(beta_0)/T ##divide by T
+  var_1_num <- var(beta_1)/T
+  
+  stdvs_0_num <- sqrt(var_0_num)
+  stdvs_1_num <- sqrt(var_1_num)
+  
+  #Let get analytical std dev, on a SMALL sample -> truuue
+  OLS_std <- OLS_own(Y,X,0)
+  res <- OLS_static$residuals
+  k <- OLS_static$param
+  
+  #We have 30 error terms per states
+  t <- 30
+  
+  #There is one different sigma per state (46 states)
+  sigma_i <- seq(1:46)
+  
+  
+  #let's compute the sigma_i 
+  for (i in 1:(length(res)/t)){
+    sigma_i[i] <- (t(res[(1+(i-1)*t):(i*t)])%*%(res[(1+(i-1)*t):(i*t)]))/(t-k) 
+  }
+  #Maybe K is 46 but not sure
+  
+  #Now we create the diagonal of sigma_hat
+  
+  sigma_est<-rep(sigma_i,each=t)
+  
+  #now we create the matrix and put sigma_hat as the diagonal of that matrix
+  
+  omega_hat <- matrix(0,length(res), length(res)) 
+  #I put back the diagonal element in the diagnonals
+  diag(omega_hat) <- sigma_est
+  
+  GLS_static = GLS_own (Y,X,omega_hat)
+  
+  stdvs_0_ana <- GLS_static[1,2]/sqrt(T)
+  stdvs_1_ana <- GLS_static[2,2]/sqrt(T)
+  
+  #estimated standard errors
+  stdvs_0_bar <- mean(stdvs_0)
+  stdvs_1_bar <- mean(stdvs_1)
+  
+  
+  #Row of estimation
+  table_beta0 <- cbind(b_0,beta_0_bar,stdvs_0_ana,stdvs_0_num,stdvs_0_bar)
+  colnames(table_beta0) <- c("population","estimated beta","analytical","numerical","estimated")
+  
+  #first row of OLS
+  table_beta1 <- cbind(b_1,beta_1_bar,stdvs_1_ana,stdvs_1_num,stdvs_1_bar)
+  colnames(table_beta1) <- c("population","estimated beta","analytical","numerical","estimated")
+  
+  
+  
+  #loop over the t-tests and give me the Critical values for each one
+  
+  #let's do a matrix of critical values
+  
+  CV_beta1_LB <- quantile(ttest_matrix[,1], c(.025))
+  CV_beta1_UB <- quantile(ttest_matrix[,1], c(.975))
+  
+  
+  #initialise matrix of rejection 
+  rej_matrix <- matrix(0,nrow(ttest_matrix),ncol(ttest_matrix))
+  #Lets store the 1 and 0 of rejection in a matrix
+  for (j in 1:5){
+    rej_matrix[,j]<-sapply(ttest_matrix[,j],rej_function, LB= CV_beta1_LB,UB= CV_beta1_UB)
+  }
+  #this is the size, the mean of column 1 for which beta =1
+  size_beta1 <- mean(rej_matrix[,1])
+  #size_beta1
+  
+  #the power is P(non reject if Beta != 1) -> 1 - P(reject)
+  power_beta1 <- 1 -colMeans(rej_matrix[,2:5])
+  #power_beta1
+  
+  
+  #Store the result for OLS with White
+  #table with size and power
+  sp_mat[4,] <- cbind(size_beta1,t(power_beta1))
+  
+  #table beta0
+  beta_0_mat[4,] <- table_beta0 
+  
+  
+  #table beta1
+  beta_1_mat[4,] <-table_beta1
+  
+  
+  
   
   
   
