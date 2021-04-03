c2f74409a23b33ee71d04503cf5b7680f6e26d7f
Pablo Vicente Pastor Y Camarasa <Pablo.Pastor@UGent.be>
2021-04-03 11:33:58 +0200

    Case 2.1 checked!

--- Case2.1_code.R
+++ Case2.1_code.R
@@ -77,17 +77,19 @@ X_bar <- rep(0,repl)
 X_var  <- rep(0,repl)
 y_bar <- rep(0,repl)
 
-#initialising the parameters value
+#Coefficients estimates
 b_0 <- 10
 b_1 <- 1
 sigma2 <- 1
-#draw only once the X from a normal distribution
+
+#draw only once the X from a normal distribution, X is deterministic
 xsim <- rnorm(T,0,1)
 #put the constant in X to have right dimension 
 X <- as.matrix(cbind(Cnst=1,xsim))
 #let's put the beta in matrix form
 beta <- as.matrix(rbind(b_0,b_1))
-#let's get some errors
+
+#Error terms are normally distributed
 e <- rnorm(T,0,sigma2)
 
 #now I can have my y !
@@ -110,7 +112,8 @@ ttest_matrix <- matrix(0,repl, length(beta1_test))
 for (j in 1:length(beta1_test)) {
 
   for (i in 1:repl) {
-  #stochastic X: X = rnorm(T,0,sigma2)
+  #stochastic X: 
+    #X = rnorm(T,0,sigma2)
   #let's get some errors, we define sigma 2 =1 earlier
   e <- rnorm(T,0,sigma2)
   #now I can have my y !
@@ -122,29 +125,37 @@ for (j in 1:length(beta1_test)) {
   beta_0[i] <- OLS_out$estimation[1,1]
   beta_1[i] <- OLS_out$estimation[2,1]
   
-  print(OLS_out$estimation[,2])
-  
-  stdvs_0[i] <- OLS_out$estimation[1,2]/sqrt(T)
-  stdvs_1[i] <- OLS_out$estimation[2,2]/sqrt(T)
+  #Estimated standard errors given by OLS
+  stdvs_0[i] <- OLS_out$estimation[1,2]
+  stdvs_1[i] <- OLS_out$estimation[2,2]
   
   ttest_matrix[i,j] <- (beta_1[i] - beta1_test[j])/(OLS_out$estimation[2,2]/sqrt(T)) #divided by sqrt T
   }
 
 }
 
-colnames(ttest_matrix) <- c(1,0.95,0.90,0.75,0.5)
+####
+#We want to check that the estimated standard error is unbiased
+#A. Estimated standard error#
+####
+
+stdvs_0_est <- mean(stdvs_0)
+stdvs_1_est <- mean(stdvs_1)
+
+var_0_est <- mean(stdvs_0)^2
+var_1_est <- mean(stdvs_1)^2
 
 
-beta_0_bar <- mean(beta_0)
-beta_1_bar <- mean(beta_1)
+beta_0_est <- mean(beta_0)
+beta_1_est <- mean(beta_1)
 
-#let's get the numerical standard errors -> truuuue
+####
+#B. True standard errors
+####
 
-var_0_num <- var(beta_0)/T ##divide by T?
-var_1_num <- var(beta_1)/T
-
-stdvs_0_num <- sqrt(var_0_num)
-stdvs_1_num <- sqrt(var_1_num)
+###
+#B.1 Use an analytical formula
+###
 
 #Let get analytical std dev, on a SMALL sample -> truuue
 OLS_std <- OLS_own(Y,X,0)
@@ -154,42 +165,48 @@ stdvs_1_ana <- OLS_std$estimation[2,2]/sqrt(T)
 
 x<-X
 xxi    <- solve(t(x)%*%x) #this is (X' X)^(-1)
-var_01_ana  <- sigma2*diag(xxi)/T
+var_01_ana  <- sigma2*(xxi)
+var_01_vec <- diag(var_01_ana)
+
 var_0_ana <- var_01_ana[1]
 var_1_ana <- var_01_ana[2]
 
 stdvs_1_ana_test <- sqrt(var_1_ana)
 
-#estimated standard errors
-stdvs_0_bar <- mean(stdvs_0)
-stdvs_1_bar <- mean(stdvs_1)
 
-var_0_bar <- mean(stdvs_0)^2
-var_1_bar <- mean(stdvs_1)^2
+
+
+var_0_num <- var(beta_0)/T ##divide by T?
+var_1_num <- var(beta_1)/T
+
+stdvs_0_num <- sqrt(var_0_num)
+stdvs_1_num <- sqrt(var_1_num)
+
 
 
 
 #let's see what are the true standard standars
-table_std <- rbind(cbind(stdvs_0_ana,stdvs_0_num,stdvs_0_bar),cbind(stdvs_1_ana,stdvs_1_num,stdvs_1_bar))
+table_std <- rbind(cbind(stdvs_0_ana,stdvs_0_num,stdvs_0_est),cbind(stdvs_1_ana,stdvs_1_num,stdvs_1_est))
 colnames(table_std) <- c("analytical","numerical","estimated")
 table_std
 
 #let's see what are the true variance
-table_var <- rbind(cbind(var_0_ana,var_0_num,var_0_bar),cbind(var_1_ana,var_1_num,var_1_bar))
+table_var <- rbind(cbind(var_0_ana,var_0_num,var_0_est),cbind(var_1_ana,var_1_num,var_1_est))
 colnames(table_var) <- c("analytical","numerical","estimated")
 table_var
 
 
 #let's do a little table that compares the MC with the population
-table_beta <- cbind(b_0,beta_0_bar,b_1,beta_1_bar)
+table_beta <- cbind(b_0,beta_0_est,b_1,beta_1_est)
 
 colnames(table_beta) <- c("Beta_0_pop","Beta_0_MC","Beta_1_pop","Beta_1_MC")
 table_beta <- cbind(T,table_beta)
 
 
 #loop over the t-tests and give me the Critical values for each one
+colnames(ttest_matrix) <- c(1,0.95,0.90,0.75,0.5)
 
-#let's do a matrix of critical values
+#let's do a matrix of critical values, alpha is 5%
 
 CV_beta1_LB <- quantile(ttest_matrix[,1], c(.025))
 CV_beta1_UB <- quantile(ttest_matrix[,1], c(.975))
@@ -219,10 +236,10 @@ power_beta1 <- 1 -colMeans(rej_matrix[,2:5])
 #power_beta1
 
 #this is the table in which you have all beta_0
-table_final_beta0[iter,] <- cbind(T,b_0,beta_0_bar,stdvs_0_ana,stdvs_0_num,stdvs_0_bar,var_0_ana,var_0_bar)
+table_final_beta0[iter,] <- cbind(T,b_0,beta_0_est,stdvs_0_ana,stdvs_0_num,stdvs_0_est,var_0_ana,var_0_est)
 
 #this is the table in which you have all beta_1
-table_final_beta1[iter,] <- cbind(T,b_1,beta_1_bar,stdvs_1_ana,stdvs_1_num,stdvs_1_bar,var_1_ana,var_1_bar,CV_beta1_LB,CV_beta1_UB)
+table_final_beta1[iter,] <- cbind(T,b_1,beta_1_est,stdvs_1_ana,stdvs_1_num,stdvs_1_est,var_1_ana,var_1_est,CV_beta1_LB,CV_beta1_UB)
 
 #table with size and power
 table_final_sp[iter,] <- cbind(T,size_beta1,t(power_beta1))
@@ -230,36 +247,3 @@ table_final_sp[iter,] <- cbind(T,size_beta1,t(power_beta1))
 }
 
 
-
-
-
-
-
-
-####################################################################
-### Bootstrap simulation for mean of Chi2 distributed variable
-####################################################################
-
-set.seed(123)
-n = 10
-repl = 100000
-
-data = read.table("data_sim1.csv")
-X <- data[1:10,1]
-
-X_bar = rep(0,repl)
-X_var  = rep(0,repl)
-
-for (i in 1:repl) {
-  sample(X,replace = TRUE)
-  X_bar[i] = mean(X)
-  X_var[i] = var(X)/n
-}
-
-mean_X_bar = mean(X_bar)
-var_X_bar  = var(X_bar)
-mean_est_var_X_bar = mean(X_var)
-skew_X_bar = mean(((X_bar-mean_X_bar)/sqrt(var_X_bar))^3)
-kurt_X_bar = mean(((X_bar-mean_X_bar)/sqrt(var_X_bar))^4)
-jb_X_bar = (repl/6)*(skew_X_bar^2 + (1/4)*(kurt_X_bar-3)^2)
-p_jb_X_bar = pchisq(jb_X_bar, df=2, lower.tail=FALSE)
