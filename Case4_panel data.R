bc4bf01e482d4a6afecec93084ae67b97159222c
Pablo Vicente Pastor Y Camarasa <Pablo.Pastor@UGent.be>
2021-04-30 18:12:03 +0200

    Panel data with FE and time FE ok

--- Case4_panel data.R
+++ Case4_panel data.R
@@ -38,17 +38,92 @@ source("Case4_FE estimator.R")
 source("Case4_GMMd estimator.R")
 
 ####################################################################
-#Case 4.1. Monte Carlo simulation for OLS, X deterministic ########
+#Case 4.1. Panel data estimator                             ########
 ####################################################################
 # Read from file
 data  <- read_excel("Data_Baltagi.xlsx")
-Pdata <- pdata.frame(data, index=c("state", "year"))
 
-#Estimate the dynamic model in levels using the FE estimator
-#create lag of lnC_it
-lag_lnC_it<-lag(Pdata$ln.C_it, k=1)
-#remove year of 1963 because NA 
+lg <- function(x)c(NA, x[1:(length(x)-1)])
+
+data$ln.C_it_1 <- unlist(tapply(data$`ln C_it`, data$state, lg))
+
+data  <- na.omit(data)
+
+#We create the matrix D of dummies for each variable
+
+y <- as.matrix(data[,10])
+
+x <- as.matrix(data[,11:14])
 
 
+idx <- sort(unique(data$state))
+state_i <- matrix(0, nrow = nrow(data), ncol = length(idx))
 
+for (j in 1:length(idx)) { 
+  state_i[,j] <- as.integer(data$state == idx[j])
+}
+
+idx <- sort(unique(data$year))
+year_i <- matrix(0, nrow = nrow(data), ncol = length(idx))
+
+for (j in 1:length(idx)) { 
+  year_i[,j] <- as.integer(data$year == idx[j])
+}
+
+#Let's write the demeaning matrix by individuals
+#I need the identity matrix
+NT <- nrow(data)
+I   <- matrix(0, NT, NT)
+diag(I) <- 1
+
+P_i <- state_i %*% solve(t(state_i) %*% state_i) %*% t(state_i)
+
+P_T <- year_i %*% solve(t(year_i) %*% year_i) %*% t(year_i)
+
+y_demean <- y - P_i %*% y
+
+x_demean <- x - P_i %*% x
+
+
+y_demean_it <- y_demean - P_T %*% y_demean
+
+x_demain_it <- x_demean - P_T %*% x_demean
+
+
+n  <- length(y)
+k  <- ncol(x)
+df <- n-k
+
+## Run OLS
+x <- x
+y <- y
+
+xy     <- t(x)%*%y
+xxi    <- solve(t(x)%*%x) #this is (X' X)^(-1)
+coefs  <- as.vector(xxi%*%xy)
+
+yhat   <- as.vector(x%*%coefs)
+res    <- y-yhat
+#sigma2 <- as.vector(t(res)%*%res/df)
+sigma2 <- as.vector(t(res)%*%res)/df
+
+#case where we only want to do 
+
+  var <- sigma2*xxi
+  stdvs <- sqrt(diag(var))
   
+  tstats <- coefs/stdvs
+  pvals  <- 2*(1-pt(abs(tstats),df))
+  
+  ## Save output
+  names(coefs) <- colnames(x)
+  
+  coefs  <- round(coefs,3)
+  stdvs  <- round(stdvs,3)
+  tstats <- round(tstats,3)
+  pvals  <- round(pvals,3)
+  
+  out = rbind(coefs,stdvs,tstats,pvals)
+  out = t(out)
+
+
