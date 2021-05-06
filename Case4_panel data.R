df6dc861cfafc7d2f7199fd81c9ff02b003b6649
Pablo Vicente Pastor Y Camarasa <Pablo.Pastor@UGent.be>
2021-05-06 17:28:23 +0200

    case 4 with max lag

--- Case4_panel data.R
+++ Case4_panel data.R
@@ -207,11 +207,9 @@ for (i in (1:(T-2))) {
   
 }
 
-W_1 <- t(Z_1)%*%H%*%Z_1 #achtung !!! this still needs to be inversed
 
-Z_1 
+#Z_1 
 
-W <- W_1
 Z <- Z_1
 
 #We start at the column 30 and go by chunks of 29
@@ -286,15 +284,153 @@ W_opt <- solve(W_notinv, tol = 1e-20)
 
 gamma <- solve(t(x_fd_s) %*% Z %*% W_opt %*% t(Z) %*% x_fd_s) %*%  t(x_fd_s) %*% Z %*% W_opt %*% t(Z) %*% y_fd_s
 
+yhat   <- as.vector(x_fd_s%*%gamma)
+res    <- y-yhat
+
+sigma2 <- as.vector(t(res)%*%res)/2
+
+var_gamma <- sigma2* solve(t(x_fd_s) %*% Z %*% W_opt %*% t(Z) %*% x_fd_s)
+
+var_g <- diag(var_gamma)
+
+std_P <- sqrt(var_g[1])
+std_Pn <- sqrt(var_g[2])
+std_Y <- sqrt(var_g[3])
+std_Ct_1 <- sqrt(var_g[4])
+
+###################################################
+############Restrict the lag length###############
+##################################################
 
 
+#let's say that I want max 3 lags
+max_lag <- 3
 
-x <- x_fd
-y <- y_fd_s
+el <- max_lag-1
+#Let's loop over the states and create our big matrix
 
-xy     <- t(x)%*%y
-xxi    <- solve(t(x)%*%x) #this is (X' X)^(-1)
-coefs  <- as.vector(xxi%*%xy)
+n_inst <- max_lag*(T-2)
+
+#The chunks have size 27 !!!
+#because yi27 is the last instrument of delta_ei29
+y_1 <- y[1:(T-2)]
+
+Z_1 <- matrix(0,T-2,n_inst)
+column <-1 
+
+for (i in (1:(T-2))) {
+  
+  #Chunk is the bunch of yi that we are going to put in the Z_i matrix
+  chunk <- as.numeric(y_1[1:i])
+  #print(chunk)
+  
+  if (length(chunk) < 2){
+    Z_1[i,column] <- chunk
+    column <- column + 1
+    
+  } else {
+    
+    print(column)
+    el <- max_lag-1
+    
+    Z_1[i,column] <- tail(chunk,el)[1]
+    Z_1[i,column+1] <- tail(chunk,el)[2]
+    column <- column + 2
+  }
+}
 
 
-Z %*% W_opt 
+Z <- Z_1
+
+#We start at the column 30 and go by chunks of 29
+#but we will only loop over the Y chunks until 27 !
+#that is because yi27 will instrument delta_ei29
+for (sst in seq(30, length(y), T)) {
+  
+  print(sst)
+  #The second chunk for example goes from 30 to 
+  #30 + 27 
+  y_i <- y[sst:(sst+27)]
+  
+  #The matrix of instruments has size 27 x 378 ! 
+  #each time period we use more lags
+  Z_i <- matrix(0,T-2,n_inst)
+  column <-1 
+  
+  for (i in (1:(T-2))) {
+    chunk <- as.numeric(y_1[1:i])
+    #print(chunk)
+    
+    if (length(chunk) < 2){
+      Z_i[i,column] <- chunk
+      column <- column + 1
+      
+    } else {
+      
+      el <- max_lag-1
+      
+      Z_i[i,column] <- tail(chunk,el)[1]
+      Z_i[i,column+1] <- tail(chunk,el)[2]
+      column <- column + 2
+    }
+    
+  }
+  
+  
+  Z <- rbind(Z,Z_i)
+  
+}
+
+n_inst_var <- N*(T-2)
+
+#Imagine our big H is very big -> we try to create 
+diagonal <- 2
+offdiagonal<- -1
+H <- matrix(0,n_inst_var,n_inst_var)
+diag(H) <- diagonal
+diag(H[-1,])<-offdiagonal
+diag(H[,-1])<-offdiagonal
+H
+
+
+W_notinv <- t(Z) %*% H %*% Z 
+#we have the big W optimal
+W_opt <- solve(W_notinv)
+
+#Now we need to create the big X and Y matrix but it is not simply x and y
+
+#Remember we can only start instrumenting in period 3 until period 29 
+#this means that we take the 27 last periods for every state!
+#for every state I need to kill the first 2 periods
+data_s <- data
+
+data_s[data_s == 65] <- NA
+data_s <- na.omit(data_s)
+
+y_fd_s <- as.matrix(data_s[,15])
+x_fd_s <- as.matrix(data_s[,16:19])
+
+#we have all the ingredients we need to compute our gamma
+big_Z <- as.matrix(cbind(x_fd_s[,1],x_fd_s[,2],x_fd_s[,3],Z))
+
+Z <- big_Z
+
+W_notinv <- t(Z) %*% H %*% Z 
+#we have the big W optimal
+W_opt <- solve(W_notinv, tol = 1e-20)
+
+gamma <- solve(t(x_fd_s) %*% Z %*% W_opt %*% t(Z) %*% x_fd_s) %*%  t(x_fd_s) %*% Z %*% W_opt %*% t(Z) %*% y_fd_s
+
+yhat   <- as.vector(x_fd_s%*%gamma)
+res    <- y-yhat
+
+sigma2 <- as.vector(t(res)%*%res)/2
+
+var_gamma <- sigma2* solve(t(x_fd_s) %*% Z %*% W_opt %*% t(Z) %*% x_fd_s)
+
+var_g <- diag(var_gamma)
+
+std_P <- sqrt(var_g[1])
+std_Pn <- sqrt(var_g[2])
+std_Y <- sqrt(var_g[3])
+std_Ct_1 <- sqrt(var_g[4])
