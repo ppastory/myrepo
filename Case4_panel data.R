669f1fa5f8382a2a8a151439e4d248f01795d95b
Pablo Vicente Pastor Y Camarasa <Pablo.Pastor@UGent.be>
2021-05-16 17:59:30 +0200

    case 4 with FE panel function

--- Case4_panel data.R
+++ Case4_panel data.R
@@ -53,82 +53,9 @@ data  <- na.omit(data)
 
 #We create the matrix D of dummies for each variable
 
-y <- as.matrix(data[,10])
+FE_est = FE(data,1)
 
-x <- as.matrix(data[,11:14])
-
-
-idx <- sort(unique(data$state))
-state_i <- matrix(0, nrow = nrow(data), ncol = length(idx))
-
-for (j in 1:length(idx)) { 
-  state_i[,j] <- as.integer(data$state == idx[j])
-}
-
-idx <- sort(unique(data$year))
-year_i <- matrix(0, nrow = nrow(data), ncol = length(idx))
-
-for (j in 1:length(idx)) { 
-  year_i[,j] <- as.integer(data$year == idx[j])
-}
-
-#Let's write the demeaning matrix by individuals
-#I need the identity matrix
-NT <- nrow(data)
-I   <- matrix(0, NT, NT)
-diag(I) <- 1
-
-P_i <- state_i %*% solve(t(state_i) %*% state_i) %*% t(state_i)
-
-P_T <- year_i %*% solve(t(year_i) %*% year_i) %*% t(year_i)
-
-y_demean <- y - P_i %*% y
-
-x_demean <- x - P_i %*% x
-
-
-y_demean_it <- y_demean - P_T %*% y_demean
-
-x_demain_it <- x_demean - P_T %*% x_demean
-
-
-n  <- length(y)
-k  <- ncol(x)
-df <- n-k
-
-## Run OLS
-x <- x
-y <- y
-
-xy     <- t(x)%*%y
-xxi    <- solve(t(x)%*%x) #this is (X' X)^(-1)
-coefs  <- as.vector(xxi%*%xy)
-
-yhat   <- as.vector(x%*%coefs)
-res    <- y-yhat
-#sigma2 <- as.vector(t(res)%*%res/df)
-sigma2 <- as.vector(t(res)%*%res)/df
-
-#case where we only want to do 
-
-  var <- sigma2*xxi
-  stdvs <- sqrt(diag(var))
-  
-  tstats <- coefs/stdvs
-  pvals  <- 2*(1-pt(abs(tstats),df))
-  
-  ## Save output
-  names(coefs) <- colnames(x)
-  
-  coefs  <- round(coefs,3)
-  stdvs  <- round(stdvs,3)
-  tstats <- round(tstats,3)
-  pvals  <- round(pvals,3)
-  
-  out = rbind(coefs,stdvs,tstats,pvals)
-  out = t(out)
-
-  T <-   length(unique(data$year)) #the real T
+T <-   length(unique(data$year)) #the real T
   
   
 ##First let's first difference our data!
