e98236660cce248dea1c3b686aa7626d3c5fd193
Pablo Vicente Pastor Y Camarasa <Pablo.Pastor@UGent.be>
2021-05-26 19:04:02 +0200

    case 4 function final version

--- case4_panel_GMMd_function.R
+++ case4_panel_GMMd_function.R
@@ -4,12 +4,12 @@ dGMM = function (data,full_set,stack,max_lag){
   
   
   if (full_set == 1){
-  #Extract T and N
-  
+    #Extract T and N
+    
     N <-   length(unique(data$state))
     T <-   length(unique(data$year)) #the real T
     
-
+    
     #Let's loop over the states and create our big matrix
     
     yit <- seq(1,T-2)
@@ -31,7 +31,7 @@ dGMM = function (data,full_set,stack,max_lag){
       
       #Chunk is the bunch of yi that we are going to put in the Z_i matrix
       chunk <- as.numeric(y_1[1:i])
-
+      
       exog_var <- data[i+2,16:18]
       
       endog_exog <- append(as.numeric(y_1[1:i]),as.numeric(exog_var))
@@ -42,8 +42,6 @@ dGMM = function (data,full_set,stack,max_lag){
       }
       column <- column + i +3
     }
-    
-    Z_1 
     
     x_1 <- exog_var
     
@@ -80,9 +78,8 @@ dGMM = function (data,full_set,stack,max_lag){
         
         #Chunk is the bunch of yi that we are going to put in the Z_i matrix
         chunk <- as.numeric(y_1[1:i])
-
-        exog_var <- x_i[i,]
         
+        exog_var <- x_i[i,]
         
         endog_exog <- append(as.numeric(y_1[1:i]),as.numeric(exog_var))
         
@@ -110,6 +107,101 @@ dGMM = function (data,full_set,stack,max_lag){
     diag(H[,-1])<-offdiagonal
     
     if(stack == 1){
+      
+      
+      N <-   length(unique(data$state))
+      
+      
+      #Let's loop over the states and create our big matrix
+      
+      yit <- seq(1,T-2)
+      n_inst <- sum(yit)
+      
+      #The chunks have size 28 !!!
+      #because yi27 is the last instrument of delta_ei29
+      y_1 <- data[1:(T-2),10]
+      
+      
+      
+      #The number of columns of Z_1 is we would have without adding is 1 + 2 + .. + 27 = ninst
+      #but now for each chunk 1 + 2 + ... + 27, we had the 3 explanatory variables as instrument
+      
+      Z_1 <- matrix(0,T-2,n_inst+((T-2)*3))
+      column <-2 
+      
+      for (i in (1:(T-2))) {
+        
+        #Chunk is the bunch of yi that we are going to put in the Z_i matrix
+        chunk <- as.numeric(y_1[1:i])
+        #reverse chunk
+        chunk <- rev(chunk)
+        exog_var <- data[i+2,16:18]
+        
+        
+        endog_exog <- append(chunk,as.numeric(exog_var))
+        
+        
+        for (j in (1:length(endog_exog))) {
+          Z_1[i,(column+j-2)] <- endog_exog[j]
+        }
+        column <- column + i +3
+      }
+      
+      
+      x_1 <- exog_var
+      
+      x_i <- x_1
+      
+      
+      Z_i <- matrix(0,T-2,n_inst+((T-2)*3))
+      
+      Z <- Z_1
+      
+      
+      #We start at the column 30 and go by chunks of 29
+      #but we will only loop over the Y chunks until 27 !
+      #that is because yi27 will instrument delta_ei29
+      for (sst in seq(31, nrow(data), T)) {
+        
+        
+        #The second chunk for example goes from 30 to 
+        #30 + 27 
+        y_i <- data[sst:(sst+27),10]
+        
+        x_i <- data[(sst+2):((sst+2)+28),16:18]
+        
+        #The matrix of instruments has size 27 x 378 ! 
+        #each time period we use more lags
+        Z_i <- matrix(0,T-2,n_inst+((T-2)*3))
+        
+        column <-2 
+        #Next time I will start in row 57 which is row 59 -2 
+        #and so one the keeps increase by 1 for each loop, this -2 takes the name iter
+        
+        
+        for (i in (1:(T-2))) {
+          
+          #Chunk is the bunch of yi that we are going to put in the Z_i matrix
+          chunk <- as.numeric(y_1[1:i])
+          chunk <- rev(chunk)
+          
+          #print(chunk)
+          exog_var <- x_i[i,]
+          
+          
+          endog_exog <- append(chunk,as.numeric(exog_var))
+          
+          for (j in (1:length(endog_exog))) {
+            Z_i[i,(column+j-2)] <- endog_exog[j]
+          }
+          column <- column + i +3
+        }
+        
+        
+        Z <- rbind(Z,Z_i)
+        
+      }
+      
       #time to stack the matrix !
       
       Z[Z == 0] <- NA
@@ -161,15 +253,18 @@ dGMM = function (data,full_set,stack,max_lag){
     yhat   <- as.vector(x%*%gamma)
     res    <- y_pred-yhat
     
-    sigma2 <- as.vector(t(res)%*%res)/2
+    n  <- length(y)
+    k  <- ncol(x)
+    df <- n-k
+    
+    sigma2 <- as.vector(t(res)%*%res)/df
+    
+    #sl. 92
+    sigma2 <- sigma2/2
     
     var_gamma <- sigma2* solve(t(x) %*% Z %*% W_opt %*% t(Z) %*% x)
     
     
-    #std_P <- sqrt(var_g[1])
-    #std_Pn <- sqrt(var_g[2])
-    #std_Y <- sqrt(var_g[3])
-    #std_Ct_1 <- sqrt(var_g[4])
     
     
     stdvs_BGMM_sys  <-  sqrt(diag(var_gamma))
@@ -178,9 +273,6 @@ dGMM = function (data,full_set,stack,max_lag){
     tstats_BGMM_sys <- gamma/stdvs_BGMM_sys
     tstats_BGMM_sys <- c(tstats_BGMM_sys)
     
-    n  <- length(y)
-    k  <- ncol(x)
-    df <- n-k
     
     pvals_GGMM_sys   <- 2*(1-pt(abs(tstats_BGMM_sys),df))
     
@@ -196,13 +288,13 @@ dGMM = function (data,full_set,stack,max_lag){
     pvals  <- round(pvals_GGMM_sys,3)
     
     
-   return(out_pvals_BGMM_sys = cbind(coefs,stdvs,tstats,pvals))
-
-    } else {
+    return(out_pvals_BGMM_sys = cbind(coefs,stdvs,tstats,pvals))
+    
+  } else {
     
     N <-   length(unique(data$state))
     T <-   length(unique(data$year)) #the real T
-      
+    
     
     #el is the number of maximum instruments per period
     el <- max_lag-1
@@ -334,7 +426,7 @@ dGMM = function (data,full_set,stack,max_lag){
     diag(H) <- diagonal
     diag(H[-1,])<-offdiagonal
     diag(H[,-1])<-offdiagonal
-    H
+    
     
     
     data_reg <- na.omit(data)
@@ -360,15 +452,15 @@ dGMM = function (data,full_set,stack,max_lag){
     yhat   <- as.vector(x%*%gamma)
     res    <- y_pred-yhat
     
-    sigma2 <- as.vector(t(res)%*%res)/2
+    n  <- length(y)
+    k  <- ncol(x)
+    df <- n-k
+    
+    sigma2 <- as.vector(t(res)%*%res)/df
+    
+    sigma2 <- sigma2/2
     
     var_gamma <- sigma2* solve(t(x) %*% Z %*% W_opt %*% t(Z) %*% x)
-    
-    
-    #std_P <- sqrt(var_g[1])
-    #std_Pn <- sqrt(var_g[2])
-    #std_Y <- sqrt(var_g[3])
-    #std_Ct_1 <- sqrt(var_g[4])
     
     
     stdvs_BGMM_sys  <-  sqrt(diag(var_gamma))
@@ -377,9 +469,6 @@ dGMM = function (data,full_set,stack,max_lag){
     tstats_BGMM_sys <- gamma/stdvs_BGMM_sys
     tstats_BGMM_sys <- c(tstats_BGMM_sys)
     
-    n  <- length(y)
-    k  <- ncol(x)
-    df <- n-k
     
     pvals_GGMM_sys   <- 2*(1-pt(abs(tstats_BGMM_sys),df))
     
@@ -399,7 +488,6 @@ dGMM = function (data,full_set,stack,max_lag){
     
   }
   
-    
-    
-}
   
+  
+}
